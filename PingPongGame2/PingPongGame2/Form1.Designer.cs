cdebf60a3abcc5e3f43c9aa61aa017071c7233c8
DanielZsigar <daniel.zsigar92@gmail.com>
2017-06-15 12:39:18 +0200

    Most of the basic tasks and some of the advanced done
    Starts and ends as required;with esc it quits and with space pressed it pause the game;score is visible,each ball touch increases,on reset it becomes 0;levels introduced: on each level the paddle is smaller and the ball is faster;
    
    extra: reset button: reset all the stats and variables to the original,repositions the ball

---
+++ PingPongGame2/PingPongGame2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Score = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.paddle = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.label1);
+            this.playground.Controls.Add(this.progressBar1);
+            this.playground.Controls.Add(this.Score);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.paddle);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(1000, 446);
+            this.playground.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(887, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(110, 14);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Score.Location = new System.Drawing.Point(25, 13);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(93, 31);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "Score:";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Maroon;
+            this.ball.Location = new System.Drawing.Point(493, 50);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(17, 16);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // paddle
+            // 
+            this.paddle.BackColor = System.Drawing.Color.Teal;
+            this.paddle.Location = new System.Drawing.Point(346, 274);
+            this.paddle.Name = "paddle";
+            this.paddle.Size = new System.Drawing.Size(277, 22);
+            this.paddle.TabIndex = 0;
+            this.paddle.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(830, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 446);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox paddle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PingPongGame2/PingPongGame2/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+        public Random random = new Random();
+        public int ballSpeed = 4;
+        public int direction = 4;
+        public int score = 1;
+        public int level = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            timer1.Enabled = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            paddle.Top = playground.Bottom - (playground.Bottom / 10);
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+            ball.Left += ballSpeed;
+            ball.Top += direction;
+
+            //collison detection of the ball when it touches the paddle
+            if (ball.Bottom >= paddle.Top && ball.Bottom <= paddle.Bottom && ball.Left >= paddle.Left &&ball.Right <= paddle.Right)
+            {
+                
+                direction = -direction;
+                Score.Text ="Score: "+ score++.ToString();
+                progressBar1.Value += 10;
+            }
+
+            // changes the size of the paddle and level text if the progress bar filled 
+            if (progressBar1.Value == 50)
+            {
+                progressBar1.Value = 0;
+                paddle.Size = new Size(paddle.Size.Width -paddle.Size.Width/5, paddle.Size.Height);
+                level++;
+                label1.Text = "Level " + level;
+            }
+
+            //collision detection when the ball touches the edge of the field
+            if (ball.Left <= playground.Left)
+            {
+                ballSpeed = -ballSpeed;
+            }
+            else if (ball.Top <= playground.Top+40)
+            {
+                direction = -direction;
+            }
+            else if (ball.Right >= playground.Right)
+            {
+                ballSpeed = -ballSpeed;
+            }
+            else if(ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+
+            // collision detection when the paddle touches the right or left edge of the field
+            if(paddle.Left <= playground.Left)
+            {
+                paddle.Location = new Point(paddle.Location.X + 6,paddle.Location.Y);
+            }
+            if (paddle.Right >= playground.Right)
+            {
+                paddle.Location = new Point(paddle.Location.X - 6, paddle.Location.Y);
+            }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // controll of the paddle with arrow keys(right,left) pressing
+            if (e.KeyCode == Keys.Right)
+            {
+                paddle.Location = new Point(paddle.Location.X +100, paddle.Location.Y);
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                paddle.Location = new Point(paddle.Location.X -100, paddle.Location.Y);
+            }
+            
+            // escape afunction with key pressing
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            // pause afunction with key pressing
+            else if (e.KeyCode == Keys.Space)
+                if (timer1.Enabled.Equals(true))
+                {
+                    timer1.Enabled = false;
+                }
+                else 
+                {
+                    timer1.Enabled = true;
+                }
+            // resets the position of the ball and clear the statistics and randomly release the ball 
+            else if(e.KeyCode == Keys.R)
+            {
+                ball.Top = 340;
+                ball.Left = 700;
+                score = 0;
+                direction = 4;
+                ballSpeed = random.Next(-4, 4);
+                progressBar1.Value = 0;
+                paddle.Width = 277;
+                paddle.Height = 22;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Level "+level;
+            ball.Location = new Point(playground.Location.X+600, paddle.Location.Y -600);
+            progressBar1.Maximum = 50;
+            
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {         
+            // insrease the ball's speed if label text changes (level up)
+            if(label1.Text != "Level 1") {
+
+                if (direction.ToString().Contains("-")){
+                    direction -= 2;
+                    ballSpeed += 2;
+                }
+                else
+                {
+                    direction += 2;
+                    ballSpeed += 2;
+                }
+            }
+        }
+    }
+}
